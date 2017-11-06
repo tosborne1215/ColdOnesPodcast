c4c581e89643844247603fa87ac1cb756de45738
tosborne <tosborne1215@gmail.com>
2017-11-05 22:02:42 -0800

    Larger text fields for blog models. Will need to adjust in the future:

--- webapp/website/blog/models.py
+++ webapp/website/blog/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 
 class Post(models.Model):
-    title = models.CharField(max_length=50)
-    description = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    description = models.TextField(max_length=10000)
     pub_date = models.DateTimeField()
