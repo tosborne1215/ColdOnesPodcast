7c5221fb337a53a8a7ed2943d3a9f5f012737af9
tosborne <tosborne1215@gmail.com>
2018-01-29 19:26:04 -0800

    updated the __str__ method to return "title - date"

--- webapp/website/blog/models.py
+++ webapp/website/blog/models.py
@@ -6,3 +6,6 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(max_length=10000)
     pub_date = models.DateTimeField()
+
+    def __str__(self):
+         return self.title + " - " + self.pub_date.strftime('%c')
